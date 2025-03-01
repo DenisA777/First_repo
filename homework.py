064e85bf609a11f3251d35709b47d113adb9c6b9
DenisA777 <denisananyan333@gmail.com>
2025-03-01 15:21:02 +0200

    Add files via upload

--- homework.py
+++ homework.py
@@ -1,63 +1,63 @@
-# from datetime import datetime
+from datetime import datetime
 
 
-# def get_days_from_today(date):
-#     try:
-#         date = datetime.strptime(date, "%Y-%m-%d").date()
-#         today = datetime.today().date()
-#         delta = today - date
-#         return delta.days
+def get_days_from_today(date):
+    try:
+        date = datetime.strptime(date, "%Y-%m-%d").date()
+        today = datetime.today().date()
+        delta = today - date
+        return delta.days
     
-#     except ValueError:
-#         raise ValueError("Неправильний формат дати. Використовуйте 'РРРР-ММ-ДД'")
+    except ValueError:
+        raise ValueError("Неправильний формат дати. Використовуйте 'РРРР-ММ-ДД'")
     
 
-# result = get_days_from_today('2021-10-09')
-# print(result)
+result = get_days_from_today('2021-10-09')
+print(result)
 
 
 
 
-# import random
+import random
 
 
-# def get_numbers_ticket(min, max, quantity):
-#     if min >= 1 and max <= 1000 and min < quantity < max:
-#         numbers = random.sample(range(min, max+1), quantity)
-#         # numbers.sort()
-#         return sorted(numbers)
-#     else: 
-#         return []
+def get_numbers_ticket(min, max, quantity):
+    if min >= 1 and max <= 1000:
+        numbers = random.sample(range(min, max+1), quantity)
+        numbers.sort()
+        return sorted(numbers)
+    else: 
+        return []
 
-# lottery_numbers = get_numbers_ticket(1, 49, 9)
-# print("Ваші лотерейні числа:", lottery_numbers)
-
-
-
-# import re
+lottery_numbers = get_numbers_ticket(1, 49, 9)
+print("Ваші лотерейні числа:", lottery_numbers)
 
 
-# def normalize_phone(phone_number):
-#     formatted_phone_number = re.sub(r'[^\d+]', '', phone_number).strip()
-#     if formatted_phone_number.startswith('+'):
-#         return formatted_phone_number
-#     if formatted_phone_number.startswith('380'):
-#         return '+' + formatted_phone_number
 
-#     return '+38' + formatted_phone_number
-# raw_numbers = [
-#         "067\t123 4567",
-#         "(095) 234-5678\n",
-#         "+380 44 123 4567",
-#         "380501234567",
-#         "    +38(050)123-32-34",
-#         "     0503451234",
-#         "(050)8889900",
-#         "38050-111-22-22",
-#         "38050 111 22 11   ",
-#     ]
-# sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
-# print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
+import re
+
+
+def normalize_phone(phone_number):
+    formatted_phone_number = re.sub(r'[^\d+]', '', phone_number).strip()
+    if formatted_phone_number.startswith('+'):
+        return formatted_phone_number
+    if formatted_phone_number.startswith('380'):
+        return '+' + formatted_phone_number
+
+    return '+38' + formatted_phone_number
+raw_numbers = [
+        "067\t123 4567",
+        "(095) 234-5678\n",
+        "+380 44 123 4567",
+        "380501234567",
+        "    +38(050)123-32-34",
+        "     0503451234",
+        "(050)8889900",
+        "38050-111-22-22",
+        "38050 111 22 11   ",
+    ]
+sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
+print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
 
 
 
